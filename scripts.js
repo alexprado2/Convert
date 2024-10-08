c0e5fd9491b43716fd4a7079590b987ffb97ab9d
DEVAlexHenrique <alex.henrique1212@gmail.com>
2024-10-08 09:55:12 -0300

    Function to convert currency

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// Currency quote of the day.
+const USD = 5.51
+const EUR = 6.04
+const GBP = 7.22
+
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
@@ -11,4 +16,23 @@ amount.addEventListener("input", () => {
 // Capturing the form submit event.
 form.onsubmit = (event) => {
     event.preventDefault()
+
+    switch(currency.valeu){
+
+            case "USD":
+                convertCurrency(amount.valeu, USD,"US$")
+                break
+            case "EUR":
+                convertCurrency(amount.valeu, EUR, "€")
+                break
+            case "GBP":
+                convertCurrency(amount.valeu, GBP, "£")
+                break    
+        }    
+    }
+
+// Function to convert currency.
+
+function convertCurrency (amount, price, symbol) {
+    
 }
