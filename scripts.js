949fa1c4cef9398ceef4724c77dff898dc1ad3f3
DEVAlexHenrique <alex.henrique1212@gmail.com>
2024-10-08 09:42:38 -0300

    Capturing the form submit event

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 // Manipulating the input value to receive only numbers.
 amount.addEventListener("input", () => {
     const hasCharactersRegex = /\D+/g
     amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+// Capturing the form submit event.
+form.onsubmit = (event) => {
+    event.preventDefault()
+}
