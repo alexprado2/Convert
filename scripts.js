fa1c3a8593ced5694ea8a1c65183c7063d51c23f
DEVAlexHenrique <alex.henrique1212@gmail.com>
2024-10-08 10:33:19 -0300

    Displaying the rate of the selected currency

--- scripts.js
+++ scripts.js
@@ -3,9 +3,12 @@ const USD = 5.51
 const EUR = 6.04
 const GBP = 7.22
 
+// Obtaining form elements.
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
 
 // Manipulating the input value to receive only numbers.
 amount.addEventListener("input", () => {
@@ -17,22 +20,32 @@ amount.addEventListener("input", () => {
 form.onsubmit = (event) => {
     event.preventDefault()
 
-    switch(currency.valeu){
+    switch (currency.value){
 
             case "USD":
-                convertCurrency(amount.valeu, USD,"US$")
+                convertCurrency(amount.value, USD,"US$")
                 break
             case "EUR":
-                convertCurrency(amount.valeu, EUR, "€")
+                convertCurrency(amount.value, EUR, "€")
                 break
             case "GBP":
-                convertCurrency(amount.valeu, GBP, "£")
+                convertCurrency(amount.value, GBP, "£")
                 break    
         }    
     }
 
 // Function to convert currency.
-
 function convertCurrency (amount, price, symbol) {
-    
+    try {
+        // Displaying the rate of the selected currency.
+        description.textContent = `${symbol} 1 = ${price}`
+        // Apply the class that displays the footer.
+        footer.classList.add("show-result")
+    } catch (error) {
+        // Remove the class that displays the footer.
+        footer.classList.remove("show-result")
+        console.log(error)
+        alert("Não foi possível converter.")
+    }
 }
+
